0855916c0fec48137ef101738d23ecfd16fcca05
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-03-09 20:57:17 +0530

    Add files via upload

---
+++ Addition OF Numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{ 
+   //addition program 
+   int a;
+   int b;
+  ;
+   
+   printf("enter the value of a");
+   scanf("%d",&a);
+   printf("enter the value of b");
+   scanf("%d" ,&b);
+   
+   c=a/b;
+   printf("the value of c is %d / %d=%d ,a,b,c");
+   return 0;
+   
+}
