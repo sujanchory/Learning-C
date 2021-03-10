0ba04f231e29a5114e63c7769f7e44f804857b67
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-03-10 10:36:12 +0530

    Add files via upload

---
+++ Subtraction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,result ;
+	printf("enter the value of num1");
+	scanf("%d",&num1);
+	printf("enter the value of num2");
+	scanf("%d",&num2);
+	result=num1-num2;
+	printf("%d-%d=%d",num1,num2,result);
+	return 0;
+}
+
+
+
