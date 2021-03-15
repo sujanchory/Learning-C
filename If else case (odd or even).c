fdeb0b0a6b333a070525af78dbae000ba01f9846
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-03-15 19:03:52 +0530

    Add files via upload

---
+++ If else case (odd or even).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+	int a;
+	printf ("enter the value of a");
+	scanf ("%d",&a);
+	if (a%2==0)
+     {printf("a is an even number");}
+	else
+     {printf("a is a odd number");}
+    return 0;
+
+}
