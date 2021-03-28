2e765d19b3d7d53080a0dc186520a441b96e5cd4
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-03-28 07:49:22 +0530

    Add files via upload

---
+++ Reverse(Function).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int reverse_num(int num)
+{
+	int r,rev=0;
+	while(num)
+	{
+		r=num%10;
+		num=num/10;
+		rev=rev*10+r
+	}
+	return rev;
+}
+int main()
+{
+	int num,rev;
+	scanf("%d",&num);
+}
