8ed302e6f54e7b93213165efbcf96911ca055ae6
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-04-06 19:14:46 +0530

    Add files via upload

---
+++ Array_basic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,arr[100],size;
+	scanf("%d",&size);
+/*	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}*/
+	for(i=0;i<size;i++)
+	{
+		printf("%d ",arr[i]);
+	 } 
+	 return 0;
+}
