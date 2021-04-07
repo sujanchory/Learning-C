c5846675d64740ba3d709d3fe5c61e823d62e72b
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-04-07 10:49:59 +0530

    Update Array_basic.c

--- Array_basic.c
+++ Array_basic.c
@@ -3,10 +3,10 @@ int main()
 {
 	int i,arr[100],size;
 	scanf("%d",&size);
-/*	for(i=0;i<size;i++)
+	for(i=0;i<size;i++)
 	{
 		scanf("%d",&arr[i]);
-	}*/
+	}
 	for(i=0;i<size;i++)
 	{
 		printf("%d ",arr[i]);
