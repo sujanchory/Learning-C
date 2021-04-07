bde5f69ad6e52c6b8e7b4123c7964d232fbd7b4d
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-04-07 10:53:04 +0530

    Add files via upload

---
+++ Switch Case.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	switch(a)
+	{case 1: printf("Hello");
+	         break;
+	 case 2: printf("Hola");
+	         break;
+	default: printf("Bye Bye");}
+	return 0;
+}
