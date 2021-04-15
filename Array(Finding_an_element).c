92cb3c331e8dc9cfb618ff2eaf3e349973e570b6
Sujan Peddinti <79261811+sujanchory@users.noreply.github.com>
2021-04-15 18:58:43 +0530

    Add files via upload

---
+++ Array(Finding_an_element).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,arr[100],size,se,s=0;
+	printf("Enter the size of Array ");
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("Enter the element you want to search ");
+	scanf("%d",&se);
+	for(i=0;i<size;i++)
+	{
+		if(se==arr[i])
+		{
+			s=1;
+			printf("Element Found");
+			break;
+		}
+		else
+		{
+			printf("Element not found");
+			break;
+		}
+	 } 
+	 return 0;
+}
